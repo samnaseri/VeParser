03f951ce537566b085521649a4cc177b73a2eb4f
Sam Naseri <sam.n@live.com.au>
2012-07-12 15:49:25 +1000

    [doc] added descriptions for IInput interface

--- src/VeParser/vNext/0.Basics.cs
+++ src/VeParser/vNext/0.Basics.cs
@@ -10,8 +10,28 @@ using System.Linq;
 
 namespace VeParser_vNext
 {
+    /// <summary>
+    /// Interface defining a input source stream for VeParser.
+    /// </summary>
+    /// <typeparam name="TToken">
+    /// The type of tokens in the input stream. If you are writing a tokenizer then this usually TToken would be <see cref="char"/>, or it 
+    /// may be string or your custom Token class if you are consuming a list of tokens.
+    /// </typeparam>
     public interface IInput<TToken>
     {
+        /// <summary>
+        /// When implemented should return a token at specified point.
+        /// </summary>
+        /// <remarks>
+        /// position will start from zero and will increase one by one. However the parser may revert to an earlier position. For example when <see cref="V.Any"/> is used then
+        /// different alternative parsing grammars will be checked so, it will repeatedly start from the position in which any started, until a grammar succeed.
+        /// <example>
+        /// Consider implementing this interface for a string value as source stream. The the implementation will basically will use string indexer to return the char
+        /// value at the desired position.
+        /// </example>
+        /// </remarks>
+        /// <param name="position">The desirec position.</param>
+        /// <returns>The token at the desired position.</returns>
         TToken GetTokenAtPosition(int position);
     }
     public class ParseState<TToken>
